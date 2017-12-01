c8889984d0e152e59b679977df018438dbaf4a8a
bobrovskayaa <nataliya.bobrovskaya@phystech.edu>
2017-12-01 06:23:26 +0300

    delete old comments

--- src/api/index.js
+++ src/api/index.js
@@ -16,41 +16,14 @@ export const postLoginApi = async (data) => {
                 'Response failed: ' + response.status + ' (' + response.statusText + ')'
             ));
         }
-    
+
         // Далее будем использовать только JSON из тела ответа.
         return response.json();
     });
 }
-export const getDataApi = async (request) => {
+
+/*export const getDataApi = async (request) => {
   const URL = "https://openexchangerates.org/api/latest.json" 
   + "?app_id=a70d3267353b45f3b01b6ca393bc1992"
   return fetch(URL).then(res => res.json());
-};
-
-export const postDataApi = async (request) => {
-    const URL = ""
-    return fetch('url', {
-        method: 'POST'
-    }).then(res => res.json());
-  };
-
-/*export const postDataApi = async (request) => {
-  const URL = '';
-  return fetch('url', {
-      method: 'POST',
-      body: 'foo=bar'
-  }).then(function(response) {
-      // Стоит проверить код ответа.
-      if (!response.ok) {
-          // Сервер вернул код ответа за границами диапазона [200, 299]
-          return Promise.reject(new Error(
-              'Response failed: ' + response.status + ' (' + response.statusText + ')'
-          ));
-      }
-  
-      // Далее будем использовать только JSON из тела ответа.
-      return response.json();
-  }).then(function(data) {
-      // ... Делаем что-то с данными.
-  });
 };*/
